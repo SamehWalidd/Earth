1f38183b8827a4e53eab3807fab26ec9532e6b75
SamehWalidd <samehwalid15@gmail.com>
2024-10-02 21:18:51 +0300

    first commit

---
+++ indexx.js
@@ -0,0 +1,82 @@
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+import { getFresnelMat } from "./src/getFresnelMat.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+
+const loader = new THREE.TextureLoader();
+const starsTexture = loader.load("./imgs/stars.jpg");
+scene.background = starsTexture;
+
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+new OrbitControls(camera, renderer.domElement);
+const detail = 12;
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("./imgs/earthmap1k.jpg"),
+  specularMap: loader.load("./imgs/earthspec1k.jpg"),
+  bumpMap: loader.load("./imgs/earthbump1k.jpg"),
+  bumpScale: 0.04,
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
+
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./imgs/earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
+
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./imgs/earthcloudmap.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load('./imgs/earthcloudmap.jpg'),
+  // alphaTest: 0.3,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.003);
+earthGroup.add(cloudsMesh);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
+
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.0023;
+  glowMesh.rotation.y += 0.002;
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+} 
+window.addEventListener('resize', handleWindowResize, false);
